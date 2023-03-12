6f3d068ddbca5f88bf94f3c00dbbc2222b2b4db8
kazi-aftabur-rahman <aftab267@gmail.com>
2023-03-12 21:29:43 +0600

    init

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function About(){
+    return (
+    <div>
+    <h2>Afrin Rahman</h2>
+    <h2>Afrin Rahman</h2>
+    <h2>Afrin Rahman</h2>
+    </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Home(){
+    return  (
+   <div>
+    <h1>Kazi Aftabur</h1>
+    <h1>Kazi Aftabur</h1>
+    <h1>Kazi Aftabur</h1>
+    
+    </div>
+    )
+   
+   
+
+   
+}
+export default Home;
